bd6ded1bf82992c4bacdfcaf85c07860ac4a4715
Micael <micael11rm@ourlook.com>
2015-11-05 13:22:54 +0100

    rubens

---
+++ app/src/main/java/com/example/micael/domotica20/DisplayMessageActivity.java
@@ -0,0 +1,26 @@
+package com.example.micael.domotica20;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(Domotica.EXTRA_MESSAGE);
+
+        //create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        //set the text view as the activity layout
+        setContentView(textView);
+    }
+}
--- app/src/main/java/com/example/micael/domotica20/Domotica.java
+++ app/src/main/java/com/example/micael/domotica20/Domotica.java
@@ -3,9 +3,11 @@ package com.example.micael.domotica20;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Vibrator;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
@@ -16,23 +18,28 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
+import java.net.URI;
+import java.net.URISyntaxException;
 
 
 public class Domotica extends AppCompatActivity {
 //commit
+    public  final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
     int contador=0;
     private Button botonencendido;
+    private Button botoncambiar;
     private Camera camera;
     private TextView porcentaje;
     private SeekBar seekBar;
     private int brillo;
     private ContentResolver cResolver;
     private Window window;
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +47,8 @@ public class Domotica extends AppCompatActivity {
         setContentView(R.layout.activity_domotica);
         botonencendido = (Button) findViewById(R.id.botonLuz);
         botonencendido.setText("PULSAR");
-        botonencendido.setBackgroundColor(Color.BLUE);
+        botonencendido.setBackgroundColor(Color.TRANSPARENT);
+        botonencendido.setTextColor(Color.LTGRAY);
         seekBar = (SeekBar) findViewById(R.id.intensity);
         seekBar.setVisibility(View.INVISIBLE);
         seekBar.setMax(255);
@@ -117,4 +125,35 @@ public class Domotica extends AppCompatActivity {
         }
     }
 
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.activitynuevo);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void abrirPagina(View view) {
+        EditText editText = (EditText) findViewById(R.id.buscar);
+        String message = editText.getText().toString();
+        uri = Uri.parse("https://www.google.es/webhp?hl=es#hl=es&q="+message+"");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void abrirMapa(View view) {
+        EditText editText = (EditText) findViewById(R.id.busacarSitio);
+        String message = editText.getText().toString();
+        uri = Uri.parse("geo:0,0?q="+message+"");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void llamar(View view) {
+        EditText editText = (EditText) findViewById(R.id.esNumero);
+        String message = editText.getText().toString();
+        uri = Uri.parse("tel:"+message+"");
+        Intent intent = new Intent(Intent.ACTION_CALL,uri);
+        startActivity(intent);
+    }
 }
